0d03afae3f900056e490f8d8ae278ac8c54bc8fc
damimonge98 <69829575+damimonge98@users.noreply.github.com>
2021-03-07 23:49:36 -0300

    exercise

---
+++ findedNeedleExercise.js
@@ -0,0 +1,20 @@
+
+function findNeedle (haystack, needle) {
+    var findedNeedle = "";
+    var iterator = 0;
+    for (var i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[iterator]) {
+            findedNeedle = findedNeedle + haystack[i];
+
+            if (findedNeedle === needle) {
+                console.log((i+1)-findedNeedle.length)
+                return; 
+            }
+            iterator = iterator + 1;
+        }
+    }
+    console.log(-1)
+    
+}
+
+findNeedle ("gaga-stupidlove", "pink")
